f8a59d8ea0f87685f104c13a0cdcbd4bda23b9ee
Santiago Noe <01sistemas2019@gmail.com>
2025-08-12 18:00:12 -0500

    añadiendo rp para movil

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function descargarCV() {
     link.download = 'Mi_CV.pdf'; // Nombre con el que se guardará
     link.click();
 }
+document.querySelector('.mobile-menu-button').addEventListener('click', function() {
+    const mobileNav = document.querySelector('.mobile-nav');
+    mobileNav.classList.toggle('active');
+});
+
+// Cerrar el menú al hacer clic en cualquier enlace
+document.querySelectorAll('.mobile-nav a').forEach(link => {
+    link.addEventListener('click', () => {
+        document.querySelector('.mobile-nav').classList.remove('active');
+    });
+});
